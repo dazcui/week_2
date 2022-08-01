d3514eac702a640b5d55f6ad3142bcc6772ed2de
dazcui <david.azcui@gmail.com>
2022-08-01 19:05:24 -0400

    otra prueba

--- src/components/CardClima.jsx
+++ src/components/CardClima.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import LoadingScreen from './loadingScreen'
+import LoadingScreen from './LoadingScreen.jsx'
 
 const CardClima = ({ lat, lon }) => {
   const [weather, setWeather] = useState()
